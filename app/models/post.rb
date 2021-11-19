ac90dc21d381d8c780c4c8f8513a7b9bf55b284d
Thomas Montalto <thomasmonta98@gmail.com>
2021-11-19 22:59:24 +1100

    done with assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :content, length: { minimum: 100 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+
 end
